5706e3328509bdb075e35d27af0e1f7d3b5c25e1
Chulpyo Kim <chulpyokim@egap.co.kr>
2024-11-14 14:59:47 +0900

    fix: UserUpdate 모델을 이용하여  사용자 정보 업데이트

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ class UserCreate(BaseModel):
     name: str
     age: int
 
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+
+
 # 사용자 전체 조회
 @app.get("/users", response_model=List[User])
 def read_users():
@@ -40,14 +45,17 @@ def read_user(user_id: str):
 
 # 사용자 수정
 @app.put("/users/{user_id}", response_model=User)
-def update_user(user_id: str, name: Optional[str] = None, age: Optional[int] = None):
+def update_user(user_id: str, user_update: UserUpdate):
     user = users.get(user_id)
+
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    if name:
-        user.name = name
-    if age:
-        user.age = age
+
+    if user_update.name:
+        user.name = user_update.name
+
+    if user_update.age:
+        user.age = user_update.age
 
     return user
 
